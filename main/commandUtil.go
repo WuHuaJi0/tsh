c94dd30f3073e6a400265a6a3416ea0fb5ad5e5d
wuhuaji0 <wuhuaji0@gmail.com>
2021-08-29 00:21:24 +0800

    run the command by exec.Command

---
+++ main/commandUtil.go
@@ -0,0 +1,12 @@
+package main
+
+import "strings"
+
+func lineToCommand(line string) (string, []string) {
+	line = strings.Replace(line, "\n", "", -1) // remove the last \n
+	command := strings.Split(line, " ")
+	if len(command) > 1 {
+		return command[0], command[1:] //if this command has more than one args.
+	}
+	return command[0], nil
+}
--- main/start.go
+++ main/start.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
 	prompt()
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		command, _ := reader.ReadString('\n')
-		print(command)
+		//todo: why not syscall.forkexec is not work correctly here.
+		line, _ := reader.ReadString('\n')
+		command, args := lineToCommand(line)
+		cmd := exec.Command(command, args...)
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println("Execute Command failed:" + err.Error())
+			return
+		}
+		fmt.Println(string(output))
 		prompt()
 	}
 }

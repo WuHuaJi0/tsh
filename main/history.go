299b79632b3b1fa6ae48963e06f1658cac330fbc
wuhuaji0 <wuhuaji0@gmail.com>
2021-09-02 21:02:09 +0800

    add history as a builtin command.

---
+++ main/history.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// record the command to a history file
+func record(command string) {
+	path := os.Getenv("HOME") + "/.tsh_history"
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil && os.IsNotExist(err) {
+		_, createErr := os.Create(path)
+		if createErr != nil {
+			fmt.Println("tsh: create .tsh_history failed！")
+			return
+		}
+	}
+
+	timestamp := time.Now().Unix()
+	s := strconv.FormatInt(timestamp, 10)
+
+	_, err = file.WriteString(s + ":" + command + "\n")
+
+	if err != nil {
+		fmt.Println("tsh: record history failed!")
+	}
+}
+
+// read all history in history file
+func history() {
+	path := os.Getenv("HOME") + "/.tsh_history"
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("tsh: can not read .tsh_history")
+		return
+	}
+	all, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println("tsh: can not read .tsh_history")
+		return
+	}
+	commands := strings.Split(strings.TrimRight(string(all[:]), "\n"), "\n")
+	for i := range commands {
+		fmt.Println(strconv.Itoa(i+1) + " " + strings.Split(commands[i], ":")[1])
+	}
+}
+
+//checkout if a command is like !number, eg: !123
+func isSearchHistory(command string) (bool, int) {
+	if !strings.HasPrefix(command, "!") {
+		return false, -1
+	}
+	number := strings.Replace(command, "!", "", 1)
+	if _, err := strconv.Atoi(number); err == nil {
+		index, _ := strconv.Atoi(number)
+		return true, index
+	}
+	return false, 0
+}
+
+func getHistory(index int) string {
+	file, _ := os.Open(os.Getenv("HOME") + "/.tsh_history")
+	scanner := bufio.NewScanner(file)
+	current := 1
+	for scanner.Scan() {
+		if current == index {
+			text := scanner.Text()
+			split := strings.Split(text, ":")
+			return split[1]
+		}
+		current++
+	}
+	return "tsh: can not find such history"
+}
--- main/start.go
+++ main/start.go
@@ -7,6 +7,47 @@ import (
 	"os/exec"
 )
 
+func run(command string, args []string) {
+
+	record(command)
+
+	if command == "cd" {
+		cd(args)
+		return
+	}
+
+	if command == "history" {
+		history()
+		return
+	}
+
+	if result, index := isSearchHistory(command); result {
+		commandString := getHistory(index)
+		command, args := lineToCommand(commandString)
+		run(command, args)
+		return
+	}
+
+	_, err := exec.LookPath(command)
+	if err != nil {
+		fmt.Println("tsh:" + "command not found:" + command)
+		return
+	}
+
+	cmd := exec.Command(command, args...)
+
+	output, err := cmd.Output()
+	if err != nil {
+		code := err.(*exec.ExitError).ProcessState.ExitCode()
+		// Somethings many command may return 1 when not have enough args or received error args
+		// for example when just typing "git"  or  "git blabla" in shell.
+		if code != 1 {
+			fmt.Println("Execute Command failed:" + err.Error())
+		}
+	}
+	fmt.Print(string(output))
+}
+
 func main() {
 	prompt()
 	reader := bufio.NewReader(os.Stdin)
@@ -15,31 +56,8 @@ func main() {
 		line, _ := reader.ReadString('\n')
 		command, args := lineToCommand(line)
 
-		if command == "cd" {
-			cd(args)
-			prompt()
-			continue
-		}
+		run(command, args)
 
-		_, err := exec.LookPath(command)
-		if err != nil {
-			fmt.Println("tsh:" + "command not found:" + command)
-			prompt()
-			continue
-		}
-
-		cmd := exec.Command(command, args...)
-
-		output, err := cmd.Output()
-		if err != nil {
-			code := err.(*exec.ExitError).ProcessState.ExitCode()
-			// Somethings many command may return 1 when not have enough args or received error args
-			// for example when just typing "git"  or  "git blabla" in shell.
-			if code != 1 {
-				fmt.Println("Execute Command failed:" + err.Error())
-			}
-		}
-		fmt.Print(string(output))
 		prompt()
 	}
 }
